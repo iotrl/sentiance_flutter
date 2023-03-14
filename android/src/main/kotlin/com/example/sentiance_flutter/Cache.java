4a0e5591edcc4fe2956cc80dc923c1aa80ba3a05
swarajsingh <swaraj@iotrl.io>
2023-03-14 14:33:46 +0530

    and 7

--- android/src/main/kotlin/com/example/sentiance_flutter/Cache.java
+++ android/src/main/kotlin/com/example/sentiance_flutter/Cache.java
@@ -1,3 +1,167 @@
+// package com.example.sentiance_flutter;
+
+// import org.json.JSONObject;
+// import android.content.Context;
+// import android.content.SharedPreferences;
+// import androidx.annotation.Nullable;
+
+// class Cache {
+//     private static final String PREF_NAME = "app";
+//     private static final String KEY_AUTH_TOKEN = "token";
+//     private static final String KEY_INITIALIZE = "initialize";
+//     private static final String KEY_APP_SECRET = "app_secret";
+//     private static final String KEY_APP_ID = "app_id";
+//     private static final String KEY_USER_LINK_URL = "user_link_url";
+//     private static final String KEY_MOBILE_HEALTH_URL = "mobile_health_url";
+//     private static final String KEY_CRASH_DETECTION_URL = "crash_detection_url";
+//     private static final String KEY_USER_INSTALL_ID = "user_install_id";
+//     private static final String KEY_USER_MOBILE_HEALTH_DATA ="mobile_health_data";
+//     private static final String KEY_CUSTOMER_ID = "customer_id";
+//     private static final String KEY_USER_ID ="user_id";
+
+//     private Context mContext;
+
+    
+
+//     Cache(Context context) {
+//         mContext = context;
+//     }
+
+//     @Nullable
+//     String getKeyUserLinkUrl() {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         return sharedPreferences.getString(KEY_USER_LINK_URL, null);
+//     }
+
+//     void setKeyUserLinkUrl(String url) {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         sharedPreferences.edit().putString(KEY_USER_LINK_URL, url).apply();
+//     }
+
+//     @Nullable
+//     String getMobileHealthUrl() {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         return sharedPreferences.getString(KEY_MOBILE_HEALTH_URL, null);
+//     }
+
+//     void setMobileHealthUrl(String mobileHealthUrl) {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         sharedPreferences.edit().putString(KEY_MOBILE_HEALTH_URL, mobileHealthUrl).apply();
+//     }
+
+//     @Nullable
+//     String getCrashDetectionUrl() {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         return sharedPreferences.getString(KEY_CRASH_DETECTION_URL, null);
+//     }
+
+//     void setCrashDetectionUrl(String crashDetectionUrl) {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         sharedPreferences.edit().putString(KEY_CRASH_DETECTION_URL, crashDetectionUrl).apply();
+//     }
+
+//     @Nullable
+//     String getUserToken() {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//        EncryptUtils en = new EncryptUtils();
+//         return en.base64decode(sharedPreferences.getString(KEY_AUTH_TOKEN, null));
+//     }
+
+//     void setUserToken(String token) {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         EncryptUtils en = new EncryptUtils();
+//         sharedPreferences.edit().putString(KEY_AUTH_TOKEN,en.base64encode(token) ).apply();
+//     }
+
+//     @Nullable
+//     String getInstallId() {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         EncryptUtils en = new EncryptUtils();
+//         return en.base64decode(sharedPreferences.getString(KEY_USER_INSTALL_ID, null));
+//     }
+
+//     void setInstallId(String installId) {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         EncryptUtils en = new EncryptUtils();
+//         sharedPreferences.edit().putString(KEY_USER_INSTALL_ID,en.base64encode(installId)).apply();
+//     }
+
+//     @Nullable
+//     String getAppId() {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//           EncryptUtils en = new EncryptUtils();
+//         return en.base64decode(sharedPreferences.getString(KEY_APP_ID, null));
+//     }
+
+//     void setKeyAppId(String appId) {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//        EncryptUtils en = new EncryptUtils();
+//         sharedPreferences.edit().putString(KEY_APP_ID, en.base64encode(appId) ).apply();
+//     }
+
+//     @Nullable
+//     String getAppSecret() {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         EncryptUtils en = new EncryptUtils();
+//         return  en.base64decode(sharedPreferences.getString(KEY_APP_SECRET, null));
+//     }
+
+//     void setAppSecret(String secret) {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         EncryptUtils en = new EncryptUtils();
+//         sharedPreferences.edit().putString(KEY_APP_SECRET, en.base64encode(secret)).apply();
+//     }
+
+//     @Nullable
+//     String getInitialize() {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         return sharedPreferences.getString(KEY_INITIALIZE, null);
+//     }
+
+//     void setInitialize(String initialize) {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         sharedPreferences.edit().putString(KEY_INITIALIZE, initialize).apply();
+//     }
+
+//     void setMobileHealthData(JSONObject data)
+//     {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         sharedPreferences.edit().putString(KEY_USER_MOBILE_HEALTH_DATA,data.toString()).apply();
+//     }
+
+//     String getMobileHealthData()
+//     {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         return sharedPreferences.getString(KEY_USER_MOBILE_HEALTH_DATA, null);
+//     }
+    
+//     void setCustomerId(String customerId)
+//     {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         EncryptUtils en = new EncryptUtils();
+//         sharedPreferences.edit().putString(KEY_CUSTOMER_ID, en.base64encode(customerId)).apply(); 
+//     }
+//     String getCustomerId()
+//     {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         EncryptUtils en = new EncryptUtils();
+//         return en.base64decode(sharedPreferences.getString(KEY_CUSTOMER_ID, null));
+//     }
+//     void setUserId(String user_id)
+//     {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//          EncryptUtils en = new EncryptUtils();
+//         sharedPreferences.edit().putString(KEY_USER_ID,en.base64encode(user_id)).apply(); 
+//     }
+//     String getUserId()
+//     {
+//         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+//         EncryptUtils en = new EncryptUtils();
+//         return en.base64decode(sharedPreferences.getString(KEY_USER_ID, null));
+//     }
+// }
+
+
 package com.example.sentiance_flutter;
 
 import org.json.JSONObject;
@@ -63,53 +227,45 @@ class Cache {
     @Nullable
     String getUserToken() {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-       EncryptUtils en = new EncryptUtils();
-        return en.base64decode(sharedPreferences.getString(KEY_AUTH_TOKEN, null));
+        return sharedPreferences.getString(KEY_AUTH_TOKEN, null);
     }
 
     void setUserToken(String token) {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        EncryptUtils en = new EncryptUtils();
-        sharedPreferences.edit().putString(KEY_AUTH_TOKEN,en.base64encode(token) ).apply();
+        sharedPreferences.edit().putString(KEY_AUTH_TOKEN, token).apply();
     }
 
     @Nullable
     String getInstallId() {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        EncryptUtils en = new EncryptUtils();
-        return en.base64decode(sharedPreferences.getString(KEY_USER_INSTALL_ID, null));
+        return sharedPreferences.getString(KEY_USER_INSTALL_ID, null);
     }
 
     void setInstallId(String installId) {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        EncryptUtils en = new EncryptUtils();
-        sharedPreferences.edit().putString(KEY_USER_INSTALL_ID,en.base64encode(installId)).apply();
+        sharedPreferences.edit().putString(KEY_USER_INSTALL_ID, installId).apply();
     }
 
     @Nullable
     String getAppId() {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-          EncryptUtils en = new EncryptUtils();
-        return en.base64decode(sharedPreferences.getString(KEY_APP_ID, null));
+        return sharedPreferences.getString(KEY_APP_ID, null);
     }
 
     void setKeyAppId(String appId) {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-       EncryptUtils en = new EncryptUtils();
-        sharedPreferences.edit().putString(KEY_APP_ID, en.base64encode(appId) ).apply();
+        sharedPreferences.edit().putString(KEY_APP_ID, appId).apply();
     }
 
     @Nullable
     String getAppSecret() {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        EncryptUtils en = new EncryptUtils();
-        return  en.base64decode(sharedPreferences.getString(KEY_APP_SECRET, null));
+        return sharedPreferences.getString(KEY_APP_SECRET, null);
     }
 
     void setAppSecret(String secret) {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        EncryptUtils en = new EncryptUtils();
-        sharedPreferences.edit().putString(KEY_APP_SECRET, en.base64encode(secret)).apply();
+        sharedPreferences.edit().putString(KEY_APP_SECRET, secret).apply();
     }
 
     @Nullable
@@ -138,25 +294,21 @@ class Cache {
     void setCustomerId(String customerId)
     {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        EncryptUtils en = new EncryptUtils();
-        sharedPreferences.edit().putString(KEY_CUSTOMER_ID, en.base64encode(customerId)).apply(); 
+        sharedPreferences.edit().putString(KEY_CUSTOMER_ID, customerId).apply(); 
     }
     String getCustomerId()
     {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        EncryptUtils en = new EncryptUtils();
-        return en.base64decode(sharedPreferences.getString(KEY_CUSTOMER_ID, null));
+        return sharedPreferences.getString(KEY_CUSTOMER_ID, null);
     }
     void setUserId(String user_id)
     {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-         EncryptUtils en = new EncryptUtils();
-        sharedPreferences.edit().putString(KEY_USER_ID,en.base64encode(user_id)).apply(); 
+        sharedPreferences.edit().putString(KEY_USER_ID, user_id).apply(); 
     }
     String getUserId()
     {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        EncryptUtils en = new EncryptUtils();
-        return en.base64decode(sharedPreferences.getString(KEY_USER_ID, null));
+        return sharedPreferences.getString(KEY_USER_ID, null);
     }
 }
--- android/src/main/kotlin/com/example/sentiance_flutter/EncryptUtils.java
+++ android/src/main/kotlin/com/example/sentiance_flutter/EncryptUtils.java
@@ -1,32 +1,32 @@
-package com.example.sentiance_flutter;
+// package com.example.sentiance_flutter;
 
-import java.util.*;
-import android.util.Log;
+// import java.util.*;
+// import android.util.Log;
 
-public class EncryptUtils {
-    // public static final String DEFAULT_ENCODING = "UTF-8"; 
-    // static BASE64Encoder enc = new BASE64Encoder();
-    // static BASE64Decoder dec = new BASE64Decoder();
+// public class EncryptUtils {
+//     // public static final String DEFAULT_ENCODING = "UTF-8"; 
+//     // static BASE64Encoder enc = new BASE64Encoder();
+//     // static BASE64Decoder dec = new BASE64Decoder();
 
-    public static String base64encode(String text) {
-        try {
-            String encodedStr = Base64.getEncoder().encodeToString(text.getBytes());
-          //  Log.e("SEncription ", text +" "+ encodedStr  );
-            return encodedStr;
-        } catch (Exception e) {
-            return null;
-        }
-    }//base64encode
+//     public static String base64encode(String text) {
+//         try {
+//             String encodedStr = Base64.getEncoder().encodeToString(text.getBytes());
+//           //  Log.e("SEncription ", text +" "+ encodedStr  );
+//             return encodedStr;
+//         } catch (Exception e) {
+//             return null;
+//         }
+//     }//base64encode
 
-    public static String base64decode(String text) {
-        try {
-            byte[] actualByte = Base64.getDecoder().decode(text);
+//     public static String base64decode(String text) {
+//         try {
+//             byte[] actualByte = Base64.getDecoder().decode(text);
 
-            String decodedStr = new String(actualByte);
-           // Log.e("SEncription ", text +" "+ decodedStr  );
-            return decodedStr;
-        } catch (Exception e) {
-            return null;
-        }
-    }//base64decode
-}
+//             String decodedStr = new String(actualByte);
+//            // Log.e("SEncription ", text +" "+ decodedStr  );
+//             return decodedStr;
+//         } catch (Exception e) {
+//             return null;
+//         }
+//     }//base64decode
+// }
